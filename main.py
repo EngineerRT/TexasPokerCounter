eb50cec73d4a9fff612322b56dba1503e5585d5f
EngineerRT <enginner.rt7@gmail.com>
2022-03-04 21:19:54 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,21 @@
+table_cards = ["A_S", "J_H", "7_D", "8_S", "10_D"]
+hand_cards = ["J_D", "3_D"]
+isFlush = False
+
+suits_table = [x[-1] for x in table_cards]
+suits_hand = [i[-1] for i in hand_cards]
+
+suits = suits_table + suits_hand
+
+flush = any([suits.count(val) >= 5 for val in suits])
+
+if flush:
+    print('Flush!')
+else:
+    print('No Flush!')
+
+
+
+
+
+
